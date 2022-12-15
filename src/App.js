4de922771b300a841b41e432d67e4ecfb7514846
sojin.son <sojin.son96@gmail.com>
2022-12-15 20:43:07 +0900

    14.5 데이터 연동하기

--- src/App.js
+++ src/App.js
@@ -1,29 +1,7 @@
-import {useState} from 'react';
-import axios from 'axios';
+import NewsList from "./Components/NewsList";
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    // axios.get('https://jsonplaceholder.typicode.com/todos/1').then(response => {
-    //   setData(response.data);
-    // });
-    try{
-      const response = await axios.get(
-        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=4afe9ccd591b4330b7940a1633c17e8a',
-      );
-      setData(response.data);
-    } catch(e){
-      console.log(e);
-    }
-  }
-  return(
-    <div>
-      <div>
-        <button onClick = {onClick}>불러오기</button>
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data,null,2)} readOnly={true}/>}
-    </div>
-  );
+  return <NewsList />
 };
 
 export default App;
